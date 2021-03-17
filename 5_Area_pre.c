bb3c89ff6a7d81a0e78cae841b740d04951500c0
Anikaitsingh <2020A1R142@mietjammu.in>
2021-03-17 20:04:04 +0530

    Area and perimeter of Circle, Square and Rectangle

---
+++ 5_Area_pre.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+	float raduisOfCircle, sideOfSquare, legthOfRectangle, breadthOfRectangle;
+	scanf("%f %f %f %f", &raduisOfCircle, &sideOfSquare, &legthOfRectangle, &breadthOfRectangle);
+	printf("Area of Circle: %f",((22*raduisOfCircle*raduisOfCircle)/7));
+	printf("\nPerimeter of Circle: %f",((2*22*raduisOfCircle)/7));
+	printf("\nArea of Square: %f",(sideOfSquare*sideOfSquare));
+	printf("\nPerimeter of Square: %f",(4*sideOfSquare));
+	printf("\nArea of Rectangle: %f",(legthOfRectangle*breadthOfRectangle));
+	printf("\nPerimeter of Rectangle: %f",(2*(legthOfRectangle+breadthOfRectangle)));
+	return 0;
+}          
