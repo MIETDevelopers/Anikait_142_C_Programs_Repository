f01b304e0a64064c830940380e272d17ac8bd1d4
Anikaitsingh <2020A1R142@mietjammu.in>
2021-03-18 21:19:58 +0530

    Check whether the alphabet is a vowel or consonant

---
+++ 8_Vowel_Consonant.c
@@ -0,0 +1,24 @@
+#include <stdio.h>  //Pre processor directive to inclide standard input and output function header file.
+
+int main(){ //Start of the main function body.
+    char ch;
+    printf("Enter any character: ");
+    scanf("%c", &ch);   //Taking input character from user
+
+    // Condition for vowel 
+    if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' || ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U')
+    {
+        printf("'%c' is Vowel.", ch);
+    }
+    else 
+        if((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))     // Condition for consonant
+        {
+            printf("'%c' is Consonant.", ch);
+        }
+        else
+        {
+            printf("'%c' is not an alphabet.", ch); //If it is neither vowel nor consonant,then it is not an alphabet.
+         
+        }
+    return 0;   //Return statment.
+}   //End of the main function body.
