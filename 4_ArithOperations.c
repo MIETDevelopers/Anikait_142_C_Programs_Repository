7271162be23703bfed8929317c8f10398c47fe9b
Anikaitsingh <2020A1R142@mietjammu.in>
2021-03-21 22:21:32 +0530

    Arithmatic Operations

--- 4_ArithOperations.c
+++ 4_ArithOperations.c
@@ -1,16 +1,14 @@
-#include <stdio.h>
-
-int main()
-{
-	char operator;
-	double n1, n2;
-	printf("Enter an operator (+, -, *, /): ");
-	scanf("%c", &operator);
+#include <stdio.h>	//Pre processor directive to include standard input output function header file.	
+int main(){	//Start of the main function body.
+	char operator;	//Variable decleration of data type character.
+	double n1, n2;	//Variable decleration of data type double.
+	printf("Enter an operator (+, -, *, /): ");	//printf function call for entring an operator.                                                                                                                                                                                                               
+	scanf("%c", &operator);	//Taking input from user.
 	printf("Enter first operand:");
 	scanf("%lf",&n1);
 	printf("Enter second operand:");
 	scanf("%lf",&n2);
-	switch(operator)
+	switch(operator)	//switch operator for calculating two number given by user according to entered operator. 
 	{
 		case '+':
 			printf("%.1lf + %.1lf = %.1lf", n1 , n2 , n1+n2);
@@ -27,5 +25,5 @@ int main()
 		default:
 			printf("Error operator is not correct");
 	}
-	return 0;
-}
+	return 0;	//Retuen statement.
+}	//End of main function body.
