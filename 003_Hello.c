d1ef60718a1e23232057f62fd2d7bbc1b8aff30c
Anikaitsingh <2020A1R142@mietjammu.in>
2021-03-22 12:37:01 +0530

    Hello world program

--- 003_Hello.c
+++ 003_Hello.c
@@ -1,5 +1,5 @@
 #include<stdio.h>	//Pre processor directive to include standard input and output function header file.	
 int main(){	//Start of the main body.
-	printf("Hello World");	//printf statment to print "Hello World".
+	printf("Hello World");	//printf function call to print "Hello World".
 	return 0;	//Return statment.
 }	//End of the main body.
