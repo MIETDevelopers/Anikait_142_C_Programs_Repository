6167a1d3700eea92bba9c22760e8edc5960f9b88
Anikaitsingh <2020A1R142@mietjammu.in>
2021-03-29 18:03:10 +0530

    Area and Perimeter of Circle, Square and Rectangle.

--- 005_Area_pre.c
+++ 005_Area_pre.c
@@ -1,3 +1,7 @@
+//Author: Anikait Singh
+//Creation date: 28 Mar,2021
+//Purpose: Area and Perimeter of Circle, Square and Rectangle.
+
 #include<stdio.h>	//Pre processor directive to include standard input and output function header file.	
 int main(){	//Start of the main function body.
 	float raduisOfCircle, sideOfSquare, legthOfRectangle, breadthOfRectangle;	//Variables decleration of data type float.
