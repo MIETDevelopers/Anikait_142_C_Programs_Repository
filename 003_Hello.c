4125f5f5b240fc1ffef02afa3bd8279729c83445
Anikaitsingh <2020A1R142@mietjammu.in>
2021-03-29 18:01:36 +0530

    Hello World Program

--- 003_Hello.c
+++ 003_Hello.c
@@ -1,3 +1,7 @@
+//Author: Anikait Singh
+//Creation date: 28 Mar,2021
+//Purpose: Hello World Program.
+
 #include<stdio.h>	//Pre processor directive to include standard input and output function header file.	
 int main(){	//Start of the main body.
 	printf("Hello World");	//printf function call to print "Hello World".
