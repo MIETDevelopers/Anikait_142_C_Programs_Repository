0200a3f649c86e776540bc25a06a5d54131c7e24
Anikaitsingh <2020A1R142@mietjammu.in>
2021-03-29 19:52:18 +0530

    To demonstrate the use of malloc(), calloc() and free() functions.

---
+++ 026_malloc_calloc_free.c
@@ -0,0 +1,141 @@
+//Author: Anikait Singh
+//Creation date: 28 Mar,2021
+//Purpose: To demonstrate the use of malloc(), calloc() and free() functions.
+
+//malloc()
+#include <stdio.h>
+#include <stdlib.h>
+  
+int main()
+{
+  
+    // This pointer will hold the
+    // base address of the block created
+    int* ptr;
+    int n, i;
+  
+    // Get the number of elements for the array
+    n = 5;
+    printf("Enter number of elements: %d\n", n);
+  
+    // Dynamically allocate memory using malloc()
+    ptr = (int*)malloc(n * sizeof(int));
+  
+    // Check if the memory has been successfully
+    // allocated by malloc or not
+    if (ptr == NULL) {
+        printf("Memory not allocated.\n");
+        exit(0);
+    }
+    else {
+  
+        // Memory has been successfully allocated
+        printf("Memory successfully allocated using malloc.\n");
+  
+        // Get the elements of the array
+        for (i = 0; i < n; ++i) {
+            ptr[i] = i + 1;
+        }
+  
+        // Print the elements of the array
+        printf("The elements of the array are: ");
+        for (i = 0; i < n; ++i) {
+            printf("%d, ", ptr[i]);
+        }
+    }
+  
+    return 0;
+}
+
+//calloc()
+#include <stdio.h>
+#include <stdlib.h>
+  
+int main()
+{
+  
+    // This pointer will hold the
+    // base address of the block created
+    int* ptr;
+    int n, i;
+  
+    // Get the number of elements for the array
+    n = 5;
+    printf("Enter number of elements: %d\n", n);
+  
+    // Dynamically allocate memory using calloc()
+    ptr = (int*)calloc(n, sizeof(int));
+  
+    // Check if the memory has been successfully
+    // allocated by calloc or not
+    if (ptr == NULL) {
+        printf("Memory not allocated.\n");
+        exit(0);
+    }
+    else {
+  
+        // Memory has been successfully allocated
+        printf("Memory successfully allocated using calloc.\n");
+  
+        // Get the elements of the array
+        for (i = 0; i < n; ++i) {
+            ptr[i] = i + 1;
+        }
+  
+        // Print the elements of the array
+        printf("The elements of the array are: ");
+        for (i = 0; i < n; ++i) {
+            printf("%d, ", ptr[i]);
+        }
+    }
+  
+    return 0;
+}
+
+//free()
+#include <stdio.h>
+#include <stdlib.h>
+  
+int main()
+{
+  
+    // This pointer will hold the
+    // base address of the block created
+    int *ptr, *ptr1;
+    int n, i;
+  
+    // Get the number of elements for the array
+    n = 5;
+    printf("Enter number of elements: %d\n", n);
+  
+    // Dynamically allocate memory using malloc()
+    ptr = (int*)malloc(n * sizeof(int));
+  
+    // Dynamically allocate memory using calloc()
+    ptr1 = (int*)calloc(n, sizeof(int));
+  
+    // Check if the memory has been successfully
+    // allocated by malloc or not
+    if (ptr == NULL || ptr1 == NULL) {
+        printf("Memory not allocated.\n");
+        exit(0);
+    }
+    else {
+  
+        // Memory has been successfully allocated
+        printf("Memory successfully allocated using malloc.\n");
+  
+        // Free the memory
+        free(ptr);
+        printf("Malloc Memory successfully freed.\n");
+  
+        // Memory has been successfully allocated
+        printf("\nMemory successfully allocated using calloc.\n");
+  
+        // Free the memory
+        free(ptr1);
+        printf("Calloc Memory successfully freed.\n");
+    }
+  
+    return 0;
+}
