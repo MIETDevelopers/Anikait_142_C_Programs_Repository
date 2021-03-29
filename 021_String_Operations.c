6121dc10efa302127636e23a38185508d3c848fd
Anikaitsingh <2020A1R142@mietjammu.in>
2021-03-29 18:00:14 +0530

    To demonstrate the use of various string operations.

---
+++ 021_String_Operations.c
@@ -0,0 +1,18 @@
+//Author: Anikait Singh
+//Creation date: 28 Mar,2021
+//Purpose: To demonstrate the use of various string operations.
+
+#include <stdio.h>		//End of main function body.//Pre-Process directive to include standard input output header file
+#include <string.h>	//Pre-Process directive to include string input output header file
+int main(){	//Start of main function body.
+    //String Declaration
+    char nickname[20];
+    //Console display using puts
+    puts("Enter your Nick name:");
+   	//Input using gets
+    gets(nickname);
+
+    puts(nickname);
+
+    return 0;	//return statment.
+}	//End of main function body.
