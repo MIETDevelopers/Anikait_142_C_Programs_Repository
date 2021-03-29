999099ffa355dd0e5bad60d899c26983d19e58eb
Anikaitsingh <2020A1R142@mietjammu.in>
2021-03-29 18:09:54 +0530

    Maximum of n numbers using for loop.

--- 012_Max_loop.c
+++ 012_Max_loop.c
@@ -1,3 +1,7 @@
+//Author: Anikait Singh
+//Creation date: 28 Mar,2021
+//Purpose: Maximum of n numbers using for loop.
+
 #include<stdio.h>   //Pre processor directive to inclide standard input and output function header file.
 int main(){ //Start of the main function body
     int n,i;    //Integer variables decleration.
