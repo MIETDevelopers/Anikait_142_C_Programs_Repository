7932a218b82972b52e73cd75a9c21040f686fa52
Anikaitsingh <2020A1R142@mietjammu.in>
2021-03-29 18:09:25 +0530

    Fibonacci sequence using for loop.

--- 011_Fibonacci_Sequence.c
+++ 011_Fibonacci_Sequence.c
@@ -1,3 +1,7 @@
+//Author: Anikait Singh
+//Creation date: 28 Mar,2021
+//Purpose: Fibonacci sequence using for loop.
+
 #include<stdio.h>	//Pre processor directive to inclide standard input and output function header file.  
 int main(){	//Start of the main function body.  
 	int x, y, i, fib , upto;	//Integer variables decleration.
