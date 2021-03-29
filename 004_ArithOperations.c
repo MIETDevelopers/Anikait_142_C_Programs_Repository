e97f00655aedb233a9b28a096e37a212936ff334
Anikaitsingh <2020A1R142@mietjammu.in>
2021-03-29 18:35:09 +0530

    Arithmetic operations using addition, subtraction, multiplication and remainder operator.

--- 004_ArithOperations.c
+++ 004_ArithOperations.c
@@ -2,32 +2,24 @@
 //Creation date: 28 Mar,2021
 //Purpose: Arithmetic operations using addition, subtraction, multiplication and remainder operator.
 
-#include <stdio.h>	//Pre processor directive to include standard input output function header file.	
-int main(){	//Start of the main function body.
-	char operator;	//Variable decleration of data type character.
-	double n1, n2;	//Variable decleration of data type double.
-	printf("Enter an operator (+, -, *, /): ");	//printf function call for entring an operator.                                                                                                                                                                                                               
-	scanf("%c", &operator);	//Taking input from user.
-	printf("Enter first operand:");
-	scanf("%lf",&n1);
-	printf("Enter second operand:");
-	scanf("%lf",&n2);
-	switch(operator)	//switch operator for calculating two number given by user according to entered operator. 
-	{
-		case '+':
-			printf("%.1lf + %.1lf = %.1lf", n1 , n2 , n1+n2);
-			break;
-		case '-':
-			printf("%.1lf - %.1lf = %.1lf", n1 , n2 , n1-n2);
-			break;
-		case '*':
-			printf("%.1lf * %.1lf = %.1lf", n1 , n2 , n1*n2);
-			break;
-		case '/':
-			printf("%.2lf / %.2lf = %.2lf", n1 , n2 , n1/n2);
-			break;
-		default:
-			printf("Error operator is not correct");
-	}
-	return 0;	//Return statement.
+#include <stdio.h>	//Pre processor directive to include standard input and output function header file.
+int main(){	//Start of main function body.
+	int num1, num2, add, subtract, multiply;  //DECLARING THE VARIABLES NAME "num1", "num2", "add", "subtract", "multiply"
+   float divide, rem;  //AND "divide" 
+ 
+   scanf("%d%d", &num1, &num2);  //READ "num1" AND "num2"
+   printf("Enter two integers = %d %d\n", num1, num2);   //PRINT THE VALUE OF "num1" AND "num2"
+ 
+   add = num1 + num2;   //ADDITION FORMULA
+   subtract = num1 - num2; //SUBTRACTION FORMULA
+   multiply = num1 * num2; //MULTIPLICATION FORMULA
+   divide = num1 / (float)num2;   //DIVISION FORMULA AND TYPECATING, YOU CAN ALSO WRITE: divide = (float)num1/num2
+   rem = num1 % num2; 
+   printf("Sum = %d\n", add);    //printf function call for printing sum of two number.
+   printf("Difference = %d\n", subtract);    //printf function call for printing difference of two number.
+   printf("Multiplication = %d\n", multiply);   //printf function call for printing multiplication of two number.
+   printf("Division = %.2f\n", divide);	//printf function call for printing division of two number.
+   printf("Remainder = %.2f\n", divide); //printf function call for printing remainder of two number.
+
+   return 0;   //Return statement
 }	//End of main function body.
