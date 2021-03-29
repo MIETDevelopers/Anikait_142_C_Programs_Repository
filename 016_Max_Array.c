8a9ecede4cbac3029eacf09781eb3b008a48efeb
Anikaitsingh <2020A1R142@mietjammu.in>
2021-03-29 18:12:09 +0530

    To find max element and index in array.

--- 016_Max_Array.c
+++ 016_Max_Array.c
@@ -1,3 +1,7 @@
+//Author: Anikait Singh
+//Creation date: 28 Mar,2021
+//Purpose: To find max element and index in array.
+
 #include <stdio.h>	//Pre processor directive to include standard input output function header file.
 int main(){	//Start of the main function body.
   	int array[100], size, c, location = 0;	//Integer variables and array declaration.
