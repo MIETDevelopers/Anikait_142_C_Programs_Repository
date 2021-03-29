dcb15c6b33a7458778f1d4619e8f603a91a1c33c
Anikaitsingh <2020A1R142@mietjammu.in>
2021-03-29 18:13:14 +0530

    Searching an element in an array using linear search.

--- 018_Array_Linear_Search.c
+++ 018_Array_Linear_Search.c
@@ -1,3 +1,7 @@
+//Author: Anikait Singh
+//Creation date: 28 Mar,2021
+//Purpose: Searching an element in an array using linear search.
+
 #include<stdio.h>   //Pre processor directive to include standard input output function header file.
 int main(){ //Start of main function body.
 	int n, i, j; //Integer variables and array declaration.
