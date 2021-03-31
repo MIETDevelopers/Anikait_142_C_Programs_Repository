c62f256b9160728ee1a1db59a87149a04104e5e7
Anikaitsingh <2020A1R142@mietjammu.in>
2021-03-31 19:20:03 +0530

    Usage of pointers.

---
+++ 038_pointer.c
@@ -0,0 +1,18 @@
+//Author: Anikait Singh
+//creation date: 28 Mar, 2021
+//Purpose: Pointer variable initialization with the address of another variable and then prints normal variable value and its address using pointer variable.
+
+#include <stdio.h>	//Preprocessor directive to include standard input output header file
+//Start of the main body function
+
+
+int main() {
+
+    float x=12.21;
+    float *y;
+    y = &x;
+    printf("Address of x = %d\n",y);
+    printf("Address of x = %.2f",*y);
+
+    return 0;   //return statement
+}
